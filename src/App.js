0debf708f589bb3b255a3d737075ab06a15f17c8
Raj Patel <rajpatel@Rajs-MacBook-Pro.local>
2021-09-17 15:09:47 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import PrivateRoute from './components/PrivateRoute';
 
 import Login from "./components/Login";
+import BubblePage from './components/BubblePage';
 import "./styles.scss";
 
 function App() {
+  const logout = () => {
+    localStorage.removeItem('token')
+  }
+
   return (
     <Router>
       <div className="App">
         <header>
           Color Picker Sprint Challenge
-          <a data-testid="logoutButton" href="#">logout</a>
+          <a onClick={logout} data-testid="logoutButton" href="http://localhost:3000/">logout</a>
         </header>
+        <Route exact path='/' component={Login} />
+        <PrivateRoute exact path='/bubblepage' component={BubblePage} />
       </div>
     </Router>
   );
@@ -23,3 +31,4 @@ export default App;
 //1. Add in two routes that link to the Login Component, one for the default path '/' and one for the '/login'.
 //2. Render BubblePage as a PrivateRoute
 //2. Build the logout button to call the logout endpoint, remove the localStorage Item and redirect to the login page.
+
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import axiosWithAuth from '../helpers/axiosWithAuth';
+import { useHistory } from 'react-router-dom';
+
+const initialValues = {username: 'Lambda', password: 'School'}
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const { push } = useHistory();
+  const [formValues, setFormValues] = useState(initialValues);
+  const [error, setError] = useState();
 
-  const error = "";
+  const handleChanges = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (formValues.username !== 'Lambda' || formValues.password !== 'School') {
+      setError('Enter valid username and password')
+    }
+
+    axiosWithAuth()
+      .post('/api/login', formValues)
+        .then((res) => {
+          console.log('Axios Login Post', res)
+          localStorage.setItem('token', res.data.payload)
+          push('/bubblepage')
+        })
+        .catch((err) => {
+          console.error({err})
+        })
+  }
+
   //replace with error state
 
   return (
@@ -13,8 +44,28 @@ const Login = () => {
       <div data-testid="loginForm" className="login-form">
         <h2>Build login form here</h2>
       </div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='username'>UserName</label>
+        <input 
+          id='username'
+          data-testid='username'
+          name='username'
+          value={formValues.username}
+          onChange={handleChanges}
+        />
+        <label htmlFor='password'>Password</label>
+        <input 
+          id='password'
+          data-testid='password'
+          name='password'
+          type='password'
+          value={formValues.password}
+          onChange={handleChanges}
+        />
+        <button>Login</button>
+      </form>
 
-      <p id="error" className="error">{error}</p>
+      <p data-testid="errorMessage" className="error">{error}</p>
     </div>
   );
 };
@@ -29,3 +80,4 @@ export default Login;
 //6. MAKE SURE YOUR USERNAME AND PASSWORD INPUTS INCLUDE id="username" and id="password"
 //7. MAKE SURE YOUR SUBMIT BUTTON INCLUDES id="submit"
 //8. MAKE SURE YOUR ERROR p tag contains the id="error"
+
